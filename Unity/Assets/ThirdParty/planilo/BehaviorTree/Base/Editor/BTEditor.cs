83a3fcc373a06f3c6960dc211e5f8cf5d38abbf3
FlameskyDexive <flamesky@live.com>
2020-12-01 23:00:47 +0800

    add node group for AI editor

--- Unity/Assets/ThirdParty/planilo/BehaviorTree/Base/Editor/BTEditor.cs
+++ Unity/Assets/ThirdParty/planilo/BehaviorTree/Base/Editor/BTEditor.cs
@@ -1,19 +1,27 @@
 using UnityEditor;
 using UnityEngine;
 using XNodeEditor;
+using XNode.NodeGroups;
 
-namespace Planilo.BT {
+namespace Planilo.BT
+{
 
-  /// <summary>Custom graph editor for BTGraph.</summary>
-  [CustomNodeGraphEditor(typeof(BTGraph))]
-  public class BTEditor : AIGraphEditor {
+    /// <summary>Custom graph editor for BTGraph.</summary>
+    [CustomNodeGraphEditor(typeof(BTGraph))]
+    public class BTEditor : AIGraphEditor
+    {
 
-    /// <summary>Show available nodes to be created for the graph.</summary>
-    public override string GetNodeMenuName(System.Type type) {
-      // Make sure type is a valid instance.
-      if (!type.IsSubclassOf(typeof(BTNode)) && !type.IsSubclassOf(typeof(BlackboardBaseNode))) { return null; }
-      // Return name as normal
-      return base.GetNodeMenuName(type).Replace("BT/", "");
+        /// <summary>Show available nodes to be created for the graph.</summary>
+        public override string GetNodeMenuName(System.Type type)
+        {
+            // Make sure type is a valid instance.
+            if (type == typeof(NodeGroup))
+            {
+                return base.GetNodeMenuName(type);
+            }
+            if (!type.IsSubclassOf(typeof(BTNode)) && !type.IsSubclassOf(typeof(BlackboardBaseNode))) { return null; }
+            // Return name as normal
+            return base.GetNodeMenuName(type).Replace("BT/", "");
+        }
     }
-  }
 }
