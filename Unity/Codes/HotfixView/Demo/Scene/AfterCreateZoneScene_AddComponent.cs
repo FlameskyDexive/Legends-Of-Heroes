8a2ff8b8cecf63282581e9ea9a973795ce14cf3e
mnfengqing <450282550@qq.com>
2022-01-13 19:11:38 +0800

    fix：修复关闭时因为UIEventCompoennt被提前释放的问题

--- Unity/Codes/HotfixView/Demo/Scene/AfterCreateZoneScene_AddComponent.cs
+++ Unity/Codes/HotfixView/Demo/Scene/AfterCreateZoneScene_AddComponent.cs
@@ -5,11 +5,12 @@ namespace ET
         protected override async ETTask Run(EventType.AfterCreateZoneScene args)
         {
             Scene zoneScene = args.ZoneScene;
-            zoneScene.AddComponent<UIEventComponent>();
-            zoneScene.AddComponent<UIPathComponent>();
             zoneScene.AddComponent<UIComponent>();
+            zoneScene.AddComponent<UIPathComponent>();
+            zoneScene.AddComponent<UIEventComponent>();
             zoneScene.AddComponent<RedDotComponent>();
             zoneScene.AddComponent<ResourcesLoaderComponent>();
+        
             zoneScene.GetComponent<UIComponent>().ShowWindow(WindowID.WindowID_Login);
             await ETTask.CompletedTask;
         }
