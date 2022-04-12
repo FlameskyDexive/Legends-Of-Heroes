b05098804838fc049911047dea3ce5066d9ec7f8
mnfengqing <450282550@qq.com>
2022-04-12 11:28:18 +0800

    feat:同步ET更新

--- Unity/Assets/Editor/EUI/UICodeSpawn/UICodeSpawner.cs
+++ Unity/Assets/Editor/EUI/UICodeSpawn/UICodeSpawner.cs
@@ -153,7 +153,8 @@ public partial class UICodeSpawner
         
         strBuilder.AppendLine("namespace ET");
         strBuilder.AppendLine("{");
-
+        strBuilder.AppendLine("\t[FriendClass(typeof(WindowCoreData))]");
+        strBuilder.AppendLine("\t[FriendClass(typeof(UIBaseWindow))]");
         strBuilder.AppendFormat("\t[AUIEvent(WindowID.WindowID_{0})]\n",strDlgName.Substring(3));
         strBuilder.AppendFormat("\tpublic  class {0}EventHandler : IAUIEventHandler\r\n", strDlgName);
           strBuilder.AppendLine("\t{");
