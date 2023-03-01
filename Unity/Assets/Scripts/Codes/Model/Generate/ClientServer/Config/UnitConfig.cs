515f899010fda5233442d487748d9f9f1bd71ea2
Flamesky <flamesky@live.com>
2023-03-01 23:27:17 +0800

    增加角色

--- Unity/Assets/Scripts/Codes/Model/Generate/ClientServer/Config/UnitConfig.cs
+++ Unity/Assets/Scripts/Codes/Model/Generate/ClientServer/Config/UnitConfig.cs
@@ -94,9 +94,9 @@ namespace ET
 		/// <summary>位置</summary>
 		[ProtoMember(4)]
 		public int Position { get; set; }
-		/// <summary>身高</summary>
+		/// <summary>模型</summary>
 		[ProtoMember(5)]
-		public int Height { get; set; }
+		public string ModelName { get; set; }
 		/// <summary>体重</summary>
 		[ProtoMember(6)]
 		public int Weight { get; set; }
