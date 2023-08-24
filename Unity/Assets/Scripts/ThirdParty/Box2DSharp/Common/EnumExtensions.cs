b19a6f9f32daa366645ee55f59cce9399c28bfe1
Flamesky <flamesky@live.com>
2023-08-24 22:16:44 +0800

    重新添加Box2DSharp, ET8的demo重写，变动太大。

---
+++ Unity/Assets/Scripts/ThirdParty/Box2DSharp/Common/EnumExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Box2DSharp.Dynamics;
+using Box2DSharp.Dynamics.Contacts;
+
+namespace Box2DSharp.Common
+{
+    public static class EnumExtensions
+    {
+        public static bool IsSet(this BodyFlags self, BodyFlags flag) => (self & flag) == flag;
+
+        public static bool IsSet(this DrawFlag self, DrawFlag flag) => (self & flag) == flag;
+
+        public static bool IsSet(this Contact.ContactFlag self, Contact.ContactFlag flag) => (self & flag) == flag;
+    }
+}
