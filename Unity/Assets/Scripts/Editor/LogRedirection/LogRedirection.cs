96aec9fa4cc299f1954b9df5a538cdcbaae92aa3
Flamesky <flamesky@live.com>
2023-12-08 20:24:25 +0800

    Fix log redirection error

--- Unity/Assets/Scripts/Editor/LogRedirection/LogRedirection.cs
+++ Unity/Assets/Scripts/Editor/LogRedirection/LogRedirection.cs
@@ -74,7 +74,7 @@ namespace ET
                 path = Path.GetFullPath(path);
             }
 #if UNITY_STANDALONE_WIN
-                        path = fullPath.Replace('/', '\\');
+                        path = path.Replace('/', '\\');
 #endif
             // 跳转到目标代码的特定行
             InternalEditorUtility.OpenFileAtLineExternal(path, line, column);
